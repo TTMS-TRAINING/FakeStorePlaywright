f24821d97c5ce00a972d848027940c0ed60d652a
AnnaJustyna <ankajrt@gmail.com>
2024-09-01 18:59:43 +0200

    LoginPage testy
    przy użyciu Cucumber

--- tests/features/login.feature
+++ tests/features/login.feature
@@ -1,6 +1,14 @@
 Feature: Account Page Login
 
-  Scenario: Login with correct email
-    Given I am on the account login Page
-    When I login with correct email credentials
-    Then I should see the dashboard welcome message
+  Scenario: Login with different email or password
+    Given I am on the account login page
+    When I login with "<accountType>"
+    Then I should see message containing: "<expectedResult>"
+    Examples:
+      | accountType        | expectedResult                                                                                                                                    |
+      | CorrectEmailLogin  | Witaj kamilakrystyna                                                                                                                              |
+      | WrongEmailLogin    | Błąd: dla adresu e-mail kamilakrystyna@gmail.com podano nieprawidłowe hasło. Nie pamiętasz hasła?                                                 |
+      | NotRegisteredEmail | Nieznany adres e-mail. Proszę sprawdzić ponownie lub wypróbować swoją nazwę użytkownika.                                                          |
+      | CorrectLogin       | Witaj kamila.socha                                                                                                                                |
+      | WrongLogin         | Błąd: wpisano niepoprawne hasło dla użytkownika kamila.socha. Nie pamiętasz hasła?                                                                |
+      | NotRegistered      | Błąd: brak kamila.testowa123 wśród zarejestrowanych w witrynie użytkowników. Jeśli nie masz pewności co do nazwy użytkownika, użyj adresu e-mail. |
--- tests/steps/loginSteps.ts
+++ tests/steps/loginSteps.ts
@@ -1,8 +1,8 @@
-import { Given, When, Then, Before, After } from '@cucumber/cucumber';
-import { expect } from 'playwright/test';
-import { AccountPage } from '../pages/AccountPage';
+import { Given, When, Then, Before, After } from "@cucumber/cucumber";
+import { expect } from "playwright/test";
+import { AccountPage } from "../pages/AccountPage";
 import { AccountTestData } from "../testData/AccountTestData";
-import { Page, Browser, chromium } from 'playwright';
+import { Page, Browser, chromium } from "playwright";
 
 let page: Page;
 let accountPage: AccountPage;
@@ -18,14 +18,41 @@ After(async function () {
   await browser.close(); // Zamknij przeglądarkę po każdym teście
 });
 
-Given('I am on the account login Page', async function () {
+Given("I am on the account login page", async function () {
   await accountPage.navigateTo("https://fakestore.testelka.pl/moje-konto/");
 });
 
-When('I login with correct email credentials', async function () {
-  await accountPage.login(AccountTestData.CorrectEmailLogin);
+When("I login with {string}", async function (accountType: string) {
+  let accountData;
+
+  switch (accountType) {
+    case "CorrectEmailLogin":
+      accountData = AccountTestData.CorrectEmailLogin;
+      break;
+    case "WrongEmailLogin":
+      accountData = AccountTestData.WrongEmailLogin;
+      break;
+    case "NotRegisteredEmail":
+      accountData = AccountTestData.NotRegisteredEmail;
+      break;
+    case "CorrectLogin":
+      accountData = AccountTestData.CorrectLogin;
+      break;
+    case "WrongLogin":
+      accountData = AccountTestData.WrongLogin;
+      break;
+    case "NotRegistered":
+      accountData = AccountTestData.NotRegistered;
+      break;
+    default:
+      throw new Error(`Account type ${accountType} is not recognized`);
+  }
+  await accountPage.login(accountData);
 });
 
-Then('I should see the dashboard welcome message', async function () {
-  await expect(page.getByText("Witaj")).toBeVisible();
-});
+Then(
+  "I should see message containing: {string}",
+  async function (expectedResult: string) {
+    await expect(page.getByText(`${expectedResult}`)).toBeVisible();
+  }
+);
