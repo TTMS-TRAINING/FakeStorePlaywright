016f9a70f99135935898b81bfad217f547e8496f
Tomasz Saganowski <saganowski.tomasz@gmail.com>
2024-09-26 22:59:21 +0200

    fix tests

--- tests/steps/loginSteps.ts
+++ tests/steps/loginSteps.ts
@@ -1,28 +1,15 @@
-import { Given, When, Then, Before, After } from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
 import { expect } from "playwright/test";
-import { AccountPage } from "../pages/AccountPage";
+import { getAccountPage, getPage } from "../hooks/testHook";
 import { AccountTestData } from "../testData/AccountTestData";
-import { Page, Browser, chromium } from "playwright";
 
-let page: Page;
-let accountPage: AccountPage;
-let browser: Browser;
-
-Before(async function () {
-  browser = await chromium.launch({ headless: false }); // Uruchom przeglądarkę przed każdym testem
-  page = await browser.newPage();
-  accountPage = new AccountPage(page);
-});
-
-After(async function () {
-  await browser.close(); // Zamknij przeglądarkę po każdym teście
-});
-
-Given("I am on the account login page", async function () {
+Given("I am on the account login page", async () => {
+  const accountPage = getAccountPage();
   await accountPage.navigateTo("https://fakestore.testelka.pl/moje-konto/");
 });
 
-When("I login with {string}", async function (accountType: string) {
+When("I login with {string}", async (accountType: string) => {
+  const accountPage = getAccountPage();
   let accountData;
 
   switch (accountType) {
@@ -50,9 +37,7 @@ When("I login with {string}", async function (accountType: string) {
   await accountPage.login(accountData);
 });
 
-Then(
-  "I should see message containing: {string}",
-  async function (expectedResult: string) {
-    await expect(page.getByText(`${expectedResult}`)).toBeVisible();
-  }
-);
+Then("I should see message containing: {string}", async (expectedResult: string) => {
+  const page = getPage();
+  await expect(page.getByText(`${expectedResult}`)).toBeVisible();
+});
